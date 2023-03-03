7336900ea9489ed4c556d1ceba1e0c99a7b5ea77
jascolil01 <jayasco246@gmail.com>
2023-03-03 16:27:38 -0500

    added seeded models

---
+++ seeders/20230303205709-user.js
@@ -0,0 +1,19 @@
+'use strict'
+const falso = require('@ngneat/falso')
+const users = [...Array(50)].map(() => ({
+  email: falso.randEmail(),
+  password: falso.randPassword(),
+  userName: falso.randUserName(),
+  name: falso.randFullName(),
+  createdAt: new Date(),
+  updatedAt: new Date()
+}))
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', users)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users')
+  }
+}
---
+++ seeders/20230303205732-comment.js
@@ -0,0 +1,20 @@
+'use strict'
+const falso = require('@ngneat/falso')
+const comments = [...Array(50)].map(() => ({
+  name: falso.randFullName(),
+  content: falso.randPhrase({ charCount: 50 }),
+  userId: falso.randNumber({min:0, max:50}),
+  postId: falso.randNumber({min:0, max:50}),
+  likeCount: falso.randNumber({min:0, max:50}),
+  createdAt: new Date(),
+  updatedAt: new Date()
+}))
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('comments', comments)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('comments')
+  }
+}
