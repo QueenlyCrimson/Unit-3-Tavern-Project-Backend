96e1bb15e5b6d5396d35967468ceaf96e521a932
QueenlyCrimson <ccinemainn@gmail.com>
2023-03-06 08:55:28 -0600

    controllers made and adjusted

---
+++ controllers/CommentController.js
@@ -0,0 +1,36 @@
+const { comment } = require("../models");
+
+const CreateComment = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
+const GetAllComment = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
+const UpdateComment = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
+const DestroyComment = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
+modules.exports = {
+  CreateComment,
+  GetAllComment,
+  UpdateComment,
+  DestroyComment,
+};
--- models/post.js
+++ models/post.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class post extends Model {
     /**
@@ -11,45 +9,48 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       post.belongsTo(models.user, {
-        foreignKey:'userId',
-        as:'posts'
-      })
+        foreignKey: "userId",
+        as: "posts",
+      });
       post.hasMany(models.comment, {
-        foreignKey:'postId',
-        as:'comments-posts'
-      })
+        foreignKey: "postId",
+        as: "comments-posts",
+      });
     }
   }
-  post.init({
-    userName: {
-      type:DataTypes.STRING,
-      allowNull:false
+  post.init(
+    {
+      userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      likeCount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        onDelete: "CASCADE",
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
     },
-    content: {
-      type:DataTypes.TEXT,
-      allowNull:false
-    },
-    img: {
-      type:DataTypes.STRING,
-      allowNull:true
-    },
-    likeCount: {
-      type:DataTypes.INTEGER,
-      allowNull:true
-    },
-    userId: {
-      type:DataTypes.INTEGER,
-      allowNull:false,
-      onDelete:'CASCADE',
-      references:{
-        model:'users',
-        key:'id'
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'post',
-    tableName:'posts'
-  });
+    {
+      sequelize,
+      modelName: "post",
+      tableName: "posts",
+    }
+  );
   return post;
 };
