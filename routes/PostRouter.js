207cdfc5fb981f8296bcfd602c44521a5e334b2f
jascolil01 <jayasco246@gmail.com>
2023-03-07 15:08:03 -0500

    added findById for user and findByUserId for post

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/PostController')
 
 Router.get('/get_posts', controller.GetAllPost)
 Router.post('/create_posts', controller.CreatePost)
+Router.get('/:user_id',controller.GetPostByUserId)
 
 module.exports = Router
--- seeders/20230303205732-comment.js
+++ seeders/20230303205732-comment.js
@@ -3,9 +3,9 @@ const falso = require('@ngneat/falso')
 const comments = [...Array(50)].map(() => ({
   name: falso.randFullName(),
   content: falso.randPhrase({ charCount: 50 }),
-  userId: falso.randNumber({min:0, max:50}),
-  postId: falso.randNumber({min:0, max:50}),
-  likeCount: falso.randNumber({min:0, max:50}),
+  userId: falso.randNumber({min:1, max:50}),
+  postId: falso.randNumber({min:1, max:50}),
+  likeCount: falso.randNumber({min:1, max:50}),
   createdAt: new Date(),
   updatedAt: new Date()
 }))
