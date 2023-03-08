ab0d81c9a642fecb668fc967d92b7fe66767f6f8
Chikodi Merenu <cmerenu@gmail.com>
2023-03-08 08:06:56 -0600

    user functions

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,20 +1,18 @@
-const { Post } = require("../models");
-
-
+const { Post } = require('../models')
 
 const GetAllPost = async (req, res) => {
   try {
-    const data = await Post.findAll();
-    res.send(data);
+    const data = await Post.findAll()
+    res.send(data)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
-const GetPostByUserId = async(req,res)=>{
+const GetPostByUserId = async (req, res) => {
   try {
     const userId = req.params.user_id
-    const data = await Post.findAll({where:{userId:userId}})
+    const data = await Post.findAll({ where: { userId: userId } })
     res.send(data)
   } catch (error) {
     throw error
@@ -24,42 +22,42 @@ const GetPostByUserId = async(req,res)=>{
 const CreatePost = async (req, res) => {
   try {
     const postBody = {
-      ...req.body,
-    };
-    let postCreate = await Post.create(postBody);
-    res.send(postCreate);
+      ...req.body
+    }
+    let postCreate = await Post.create(postBody)
+    res.send(postCreate)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const UpdatePost = async (req, res) => {
   try {
-    const postId = parseInt(req.params.postId);
+    const postId = parseInt(req.params.postId)
     let updatedPost = await Post.update(req.body, {
       where: { id: postId },
-      returning: true,
-    });
-    res.send(updatedPost);
+      returning: true
+    })
+    res.send(updatedPost)
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const DeletePost = async (req, res) => {
   try {
-    let postId = parseInt(req.params.postId);
-    await Post.destroy({ where: postId });
-    res.send({ message: `Deleted Post with an id of ${postId}` });
+    let postId = parseInt(req.params.postId)
+    await Post.destroy({ where: postId })
+    res.send({ message: `Deleted Post with an id of ${postId}` })
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 module.exports = {
   GetPostByUserId,
   GetAllPost,
   CreatePost,
   UpdatePost,
-  DeletePost,
-};
+  DeletePost
+}
