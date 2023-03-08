3b628011491fd03cdeeda1f90dd27a95d2b965f9
QueenlyCrimson <ccinemainn@gmail.com>
2023-03-08 17:13:06 -0600

    Im only committing because it wont work otherwise. github has me at the end of my rope"

--- middleware/index.js
+++ middleware/index.js
@@ -1,58 +1,60 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
-const APP_SECRET = process.env.APP_SECRET
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
+const APP_SECRET = process.env.APP_SECRET;
 
 const hashPassword = async (password) => {
-  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-  return hashedPassword
-}
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+  return hashedPassword;
+};
 
 const comparePassword = async (storedPassword, password) => {
-  let passwordMatch = await bcrypt.compare(password, storedPassword)
-  return passwordMatch
-}
+  let passwordMatch = await bcrypt.compare(password, storedPassword);
+  return passwordMatch;
+};
+
+const emailCheck = async (email) => {};
 
 const createToken = (payload) => {
-  let token = jwt.sign(payload, APP_SECRET)
-  return token
-}
+  let token = jwt.sign(payload, APP_SECRET);
+  return token;
+};
 
 const stripToken = (req, res, next) => {
   try {
-    const token = req.headers['authorization'].split(' ')[1]
+    const token = req.headers["authorization"].split(" ")[1];
     if (token) {
-      res.locals.token = token
-      return next()
+      res.locals.token = token;
+      return next();
     }
-    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+    res.status(401).send({ status: "Error", msg: "Unauthorized" });
   } catch (error) {
-    console.log(error)
-    res.status(401).send({ status: 'Error', msg: 'Strip Token Error!'})
+    console.log(error);
+    res.status(401).send({ status: "Error", msg: "Strip Token Error!" });
   }
-}
+};
 
 const verifyToken = (req, res, next) => {
-  const { token } = res.locals
+  const { token } = res.locals;
   try {
-    let payload = jwt.verify(token, APP_SECRET)
+    let payload = jwt.verify(token, APP_SECRET);
     if (payload) {
-      res.locals.payload = payload
-      return next()
+      res.locals.payload = payload;
+      return next();
     }
-    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+    res.status(401).send({ status: "Error", msg: "Unauthorized" });
   } catch (error) {
-    console.log(error)
-    res.status(401).send({ status: 'Error', msg: 'Verify Token Error!' })
+    console.log(error);
+    res.status(401).send({ status: "Error", msg: "Verify Token Error!" });
   }
-}
+};
 
 module.exports = {
   hashPassword,
   comparePassword,
   createToken,
   stripToken,
-  verifyToken
-}
+  verifyToken,
+};
