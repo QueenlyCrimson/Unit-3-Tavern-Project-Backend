90c45a801fd2d648c028a100cbb51ec77b416159
NellyNel520 <channelharris520@gmail.com>
2023-03-09 10:49:36 -0500

    corrected user and comment association

--- models/comment.js
+++ models/comment.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
     userId:{
       type:DataTypes.INTEGER,
       allowNull: false,
+      field: 'userId',
       onDelete:'CASCADE',
       references:{
         model:'users',
