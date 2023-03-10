f7d40642ed08702adca23898bac5d7095acadee2
Chikodi Merenu <cmerenu@gmail.com>
2023-03-10 08:41:52 -0600

    delete post

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -6,6 +6,5 @@ Router.get('/get_posts', controller.GetAllPost)
 Router.post('/create_posts', controller.CreatePost)
 Router.get('/by_user_id/:user_id', controller.GetPostByUserId)
 Router.get('/by_id/:id', controller.GetPostById)
-Router.delete('/:postId', controller.DeletePost)
 
 module.exports = Router
